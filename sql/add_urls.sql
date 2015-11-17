5354067cc1cedc279588c8aa94fd77c07d1b75b8
zaifoski <sofiachimirri@gmailcom>
2015-11-17 23:24:44 +0100

    previous commit fix

--- sql/add_urls.sql
+++ sql/add_urls.sql
@@ -4,16 +4,26 @@ ADD url varchar(50);
 alter table carpediem.Categoria
 ADD constraint url unique (url);
 
-insert into carpediem.Categoria (url)
-values 	'fai-da-te'
-where nome = 'Fai-da-te',
-values 	'ortaggi-e-foraggi'
-where nome = 'Ortaggi e Foraggi',
-values 	'ricette-della-nonna'
-where nome = 'Ricette della Nonna',
-values 	'rimedi-casalinghi'
-where nome = 'Rimedi Casalinghi',
-values 	'reduci-di-guerra'
-where nome = 'Reduci di Guerra',
-values 	'c-era-una-volta'
-where nome = 'C era una volta';
+UPDATE carpediem.Categoria
+SET url = 'fai-da-te'
+WHERE nome = 'Fai-da-te';
+
+UPDATE carpediem.Categoria
+SET url = 'ortaggi-e-foraggi'
+WHERE nome = 'Ortaggi e Foraggi';
+
+UPDATE carpediem.Categoria
+SET url = 'ricette-della-nonna'
+WHERE nome = 'Ricette della Nonna';
+
+UPDATE carpediem.Categoria
+SET url = 'rimedi-casalinghi'
+WHERE nome = 'Rimedi Casalinghi';
+
+UPDATE carpediem.Categoria
+SET url = 'reduci-di-guerra'
+WHERE nome = 'Reduci di Guerra';
+
+UPDATE carpediem.Categoria
+SET url = 'c-era-una-volta'
+WHERE nome = 'C era una volta';
